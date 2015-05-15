1610afd232d245a1aebdc091ed339d7e579cdacb
Fei Gao <feigao.chn@gmail.com>
2015-05-15 17:21:41 +0800

    robots.txt and humans.txt

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -31,6 +31,13 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertEqual(h1.value_of_css_property("color"),
                          "rgba(200, 50, 255, 1)")
 
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + '/robots.txt')
+        self.assertNotIn("Not Found", self.browser.title)
+
+        self.browser.get(self.live_server_url + '/humans.txt')
+        self.assertNotIn("Not Found", self.browser.title)
+
 
 def main():
     # unittest.main(warnings='ignore')  # avoid a ResourceWarning message
