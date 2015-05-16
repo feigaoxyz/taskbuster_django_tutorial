faec6300cbe7f16e34abb6a42e451bce0c9ebf2f
Fei Gao <feigao.chn@gmail.com>
2015-05-16 14:40:44 +0800

    fix url pattern

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 from .views import home, home_files
 
 
-urlpatterns = [
+urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'taskbuster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name="home-files"),
     url(r'accounts/', include('allauth.urls')),
     url(r'i18n/', include('django.conf.urls.i18n')),
-]
+)
 
 urlpatterns += i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
