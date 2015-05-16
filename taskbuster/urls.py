43edfd00dd5638746907a55404e6f034998010f2
Fei Gao <feigao.chn@gmail.com>
2015-05-16 14:24:15 +0800

    django-allauth urls

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     # url(r'^$', 'taskbuster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name="home-files")
+    '',
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name="home-files"),
+    url(r'accounts/', include('allauth.urls')),
+    url(r'i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
