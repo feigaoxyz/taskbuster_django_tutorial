554cf98e70ea2555111ee4c9ece365b36a44179a
Fei Gao <feigao.chn@gmail.com>
2015-05-16 14:12:58 +0800

    test for google login

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -4,16 +4,21 @@ from datetime import date
 
 from selenium.webdriver.firefox import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.translation import activate
 from django.utils import formats
 
+FIREFOX_URI = '/opt/homebrew-cask/Caskroom/firefox/37.0.2/Firefox.app/Contents/MacOS/firefox-bin'
+
 
 class HomeNewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
-        firefox_binary = FirefoxBinary(
-            '/opt/homebrew-cask/Caskroom/firefox/37.0.2/Firefox.app/Contents/MacOS/firefox-bin')
+        firefox_binary = FirefoxBinary(FIREFOX_URI)
         self.browser = webdriver.WebDriver(firefox_binary=firefox_binary)
         self.browser.implicitly_wait(3)
         activate('en')
@@ -68,6 +73,40 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         ny = self.browser.find_element_by_id('time-ny').text
         self.assertNotEqual(tz, utc)
         self.assertNotIn(ny, [tz, utc])
+
+
+class TestGoogleLogin(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.WebDriver(firefox_binary=FirefoxBinary(FIREFOX_URI))
+        self.browser.implicitly_wait(3)
+        self.browser.wait = WebDriverWait(self.browser, 10)
+        activate('en')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_element_by_id(self, element_id):
+        return self.browser.wait.until(expected_conditions.presence_of_element_located(By.ID, element_id))
+
+    def get_button_by_id(self, element_id):
+        return self.browser.wait.unitil(expected_conditions.element_to_be_clickable(By.ID, element_id))
+
+    def get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
+
+    def test_google_login(self):
+        self.browser.get(self.get_full_url('home'))
+        google_login = self.get_element_by_id('google_login')
+        with self.assertRaises(TimeoutException):
+            self.get_element_by_id('logout')
+
+        self.assertEqual(google_login.get_attribute('href'), self.live_server_url + '/account/google/login')
+        google_login.click()
+        with self.assertRaises(TimeoutException):
+            self.get_element_by_id('google_login')
+        google_logout = self.get_element_by_id('logout')
+        google_logout.click()
+        google_login = self.get_element_by_id('google_login')
 
 
 def main():
