55bb30a91b705f9c6e6804a7f8fcd5f1705fe6f7
Fei Gao <feigao.chn@gmail.com>
2015-05-16 13:51:40 +0800

    connect postgresql

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -87,8 +87,14 @@ WSGI_APPLICATION = 'taskbuster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskbuster_db',
+        'USER': 'taskbuster_user',
+        'PASSWORD': 'taskbuster_pwd',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
